30ee125c79928a0ac776dceabe15cda4cee72fa1
edvaldoljr <edvaldoluizj@gmail.com>
2022-02-04 22:00:45 -0300

    classe Curso

--- src/me/dio/web/projeto/Curso.java
+++ src/me/dio/web/projeto/Curso.java
@@ -1,4 +1,32 @@
 package me.dio.web.projeto;
 
-public class Curso {
+public class Curso extends Conteudo {
+
+    private int cargaHoraria;
+
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+    public Curso() {
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
 }
